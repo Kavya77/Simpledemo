a282d35f3851b6950a6322e7c4a47e66d6967a0b
kavya77 <Kavyat87@gmail.com>
2017-08-09 13:38:50 -0500

    //KAVYA ADDED NEW MESSAGE

--- src/Greetings.java
+++ src/Greetings.java
@@ -2,7 +2,7 @@ public class Greetings {
     public static void main(String[] args)
     {
         System.out.println("Good Morning");
-        System.out.println("Good Night");
+        System.out.println("Good Night");//KAVYA
     }
 
 }
